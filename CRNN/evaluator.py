b9b981e7caf086a39175ddf25c58bdb1d22b9ec8
JuanCarlosMartinezSevilla <jcms9@gcloud.ua.es>
2022-05-03 12:43:40 +0200

    a

--- CRNN/evaluator.py
+++ CRNN/evaluator.py
@@ -20,7 +20,7 @@ class ModelEvaluator:
             # aug factor == 0 means no augmentation at all
             if self.aug_factor == 0:
                 name = self.X[idx]
-                sample_image = cv2.imread(f'./dataset/e2e_crops/{name}', cv2.IMREAD_COLOR)
+                sample_image = cv2.imread(f'./dataset_crops/e2e_crops/{name}', cv2.IMREAD_COLOR)
                 sample_image = U.normalize(sample_image)
                 sample_image = U.resize(sample_image, Config.img_height)
 
